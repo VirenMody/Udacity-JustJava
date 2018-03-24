64ab8da99647a688a024c38c0e21b4aa18d2a2f2
Viren Mody <virenmody@gmail.com>
2018-03-23 22:43:23 -0500

    added proper functionality to increment decrement methods

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -21,6 +21,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +30,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * This method is called when the increment (+) button is clicked.
+     */
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the decrement (-) button is clicked.
+     */
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        display(quantity);
+    }
+
+    /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 2;
-        display(quantity);
         displayPrice(quantity * 5);
     }
 
@@ -52,14 +68,4 @@ public class MainActivity extends AppCompatActivity {
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
-
-    public void increment(View view) {
-        int quantity = 3;
-        display(quantity);
-    }
-
-    public void decrement(View view) {
-        int quantity = 1;
-        display(quantity);
-    }
 }
